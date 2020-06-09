65f1542eb7f4a38958cd34020102c717b5110947
Bonya-dk <60513252+Bonya-dk@users.noreply.github.com>
2020-06-09 11:23:20 +0200

    Create AccountRepository.java

---
+++ lab3/AccountRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<Account, Long> {
+
+  List<Account> findByNameStartingWith(String name);
+
+  List<Account> findById(long id);
+  
+  List<Account> findByBalanceBetween(int i, int j);
+}
